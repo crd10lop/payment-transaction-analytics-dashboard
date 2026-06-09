db02f085f9470881fe95c2250d0a28d030a618a4
crd10lop <cristian.diez@udea.edu.co>
2026-06-09 13:51:01 -0500

    ci: integrar pipeline de github actions con sonarcloud y pruebas e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     headless: true,
   },
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run build && npm run start',
     url: 'http://localhost:3000',
     reuseExistingServer: !!process.env.CI,
   },
